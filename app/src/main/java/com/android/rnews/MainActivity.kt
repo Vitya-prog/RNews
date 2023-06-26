848b91cd98fc5f5e3db7179267451cb62f099af1
Vitya-prog <vityadubovik120@gmail.com>
2023-06-26 19:28:27 +0300

    create a splash screen

--- app/src/main/java/com/android/rnews/MainActivity.kt
+++ app/src/main/java/com/android/rnews/MainActivity.kt
@@ -2,10 +2,13 @@ package com.android.rnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
     }
 }
